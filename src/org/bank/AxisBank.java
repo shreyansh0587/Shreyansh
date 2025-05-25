5a2dcb54e52d0926e1396a8a30e15b5d67575d45
shreyansh0587 <appwayjobs@gmail.com>
2025-05-25 10:31:59 +0530

    code added for testing

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class AxisBank extends BankInfo{
+	private void deposit() {
+		// TODO Auto-generated method stub
+		System.out.println("Axix bank provides different banking facilities");
+
+	}
+	public static void main(String[] args) {
+		AxisBank a=new AxisBank();
+		a.deposit();
+		a.saving();
+a.fixed();				
+	}
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,17 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+		// TODO Auto-generated method stub
+		System.out.println("Banking priovides savings account for everuy individuals");
+
+	}
+	public void fixed() {
+		// TODO Auto-generated method stub
+		System.out.println("Banking priovides fixed deposit for everuy individuals");
+
+
+	}
+}
+
+
